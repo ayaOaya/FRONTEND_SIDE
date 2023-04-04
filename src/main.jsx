1331cf700027e9f4f6f71281c5bb200275adab37
ayaOaya <alyaauaacn@gmail.com>
2023-04-04 02:52:12 +0200

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,19 @@ import App from './App'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
+    <Provider store={store}>
    <BrowserRouter>
    <Routes>
     <Route path='/*' element={<App />} />
    </Routes>
    </BrowserRouter>
+   </Provider>
   </React.StrictMode>,
 )
