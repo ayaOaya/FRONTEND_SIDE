f086831f324e9b935447dc17751495128181be15
ayaOaya <alyaauaacn@gmail.com>
2023-04-06 02:48:43 +0200

    roles and permissions

--- src/components/DashFooter.jsx
+++ src/components/DashFooter.jsx
@@ -1,22 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse } from "@fortawesome/free-solid-svg-icons"
-import { useNavigate, useLocation } from "react-router-dom"
+import { useNavigate, useLocation } from 'react-router-dom'
+import useAuth from "../hooks/useAuth"
 
 const DashFooter = () => {
 
+    const { username, status } = useAuth()
+
     const navigate = useNavigate()
-    const { pathname }  = useLocation()
+    const { pathname } = useLocation()
 
     const onGoHomeClicked = () => navigate('/dash')
 
     let goHomeButton = null
     if (pathname !== '/dash') {
         goHomeButton = (
-            <button 
-            className="dash-footer_button icon-button"
-            title="Home"
-            onClick={onGoHomeClicked} >
-            <FontAwesomeIcon icon={faHouse} />
+            <button
+                className="dash-footer__button icon-button"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
+                <FontAwesomeIcon icon={faHouse} />
             </button>
         )
     }
@@ -24,11 +28,10 @@ const DashFooter = () => {
     const content = (
         <footer className="dash-footer">
             {goHomeButton}
-            <p>Current User:</p>
-            <p>Status:</p>
+            <p>Current User: {username}</p>
+            <p>Status: {status}</p>
         </footer>
     )
-  return content
+    return content
 }
-
 export default DashFooter
